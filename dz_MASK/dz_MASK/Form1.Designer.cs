41227d3f0a2f7fa80feb188af263df6fa9a64cd1
Kristal <kristal.gim@mail.ru>
2024-06-09 17:22:56 +0300

    dz

---
+++ dz_MASK/dz_MASK/Form1.Designer.cs
@@ -0,0 +1,234 @@
+﻿namespace dz_MASK
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.domainUpDown1 = new System.Windows.Forms.DomainUpDown();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox_MASK = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label_IP = new System.Windows.Forms.Label();
+            this.label_SH = new System.Windows.Forms.Label();
+            this.label_KOL = new System.Windows.Forms.Label();
+            this.label_Yz = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // domainUpDown1
+            // 
+            this.domainUpDown1.Items.Add("0");
+            this.domainUpDown1.Items.Add("1");
+            this.domainUpDown1.Items.Add("2");
+            this.domainUpDown1.Items.Add("3");
+            this.domainUpDown1.Items.Add("4");
+            this.domainUpDown1.Items.Add("5");
+            this.domainUpDown1.Items.Add("6");
+            this.domainUpDown1.Items.Add("7");
+            this.domainUpDown1.Items.Add("8");
+            this.domainUpDown1.Items.Add("9");
+            this.domainUpDown1.Items.Add("10");
+            this.domainUpDown1.Items.Add("11");
+            this.domainUpDown1.Items.Add("12");
+            this.domainUpDown1.Items.Add("13");
+            this.domainUpDown1.Items.Add("14");
+            this.domainUpDown1.Items.Add("15");
+            this.domainUpDown1.Items.Add("16");
+            this.domainUpDown1.Items.Add("17");
+            this.domainUpDown1.Items.Add("18");
+            this.domainUpDown1.Items.Add("19");
+            this.domainUpDown1.Items.Add("20");
+            this.domainUpDown1.Items.Add("21");
+            this.domainUpDown1.Items.Add("22");
+            this.domainUpDown1.Items.Add("23");
+            this.domainUpDown1.Items.Add("24");
+            this.domainUpDown1.Items.Add("25");
+            this.domainUpDown1.Items.Add("26");
+            this.domainUpDown1.Items.Add("27");
+            this.domainUpDown1.Items.Add("28");
+            this.domainUpDown1.Items.Add("29");
+            this.domainUpDown1.Items.Add("30");
+            this.domainUpDown1.Items.Add("31");
+            this.domainUpDown1.Items.Add("32");
+            this.domainUpDown1.Location = new System.Drawing.Point(203, 70);
+            this.domainUpDown1.Name = "domainUpDown1";
+            this.domainUpDown1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.domainUpDown1.Size = new System.Drawing.Size(45, 20);
+            this.domainUpDown1.TabIndex = 0;
+            this.domainUpDown1.Click += new System.EventHandler(this.domainUpDown1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(82, 38);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox_MASK
+            // 
+            this.textBox_MASK.Location = new System.Drawing.Point(82, 70);
+            this.textBox_MASK.Name = "textBox_MASK";
+            this.textBox_MASK.Size = new System.Drawing.Size(100, 20);
+            this.textBox_MASK.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(2, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(74, 18);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Address:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(2, 68);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 18);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Mask:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(5, 108);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(80, 15);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Адресс сети:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(5, 128);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(179, 15);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Широковещательный адресс:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(5, 150);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(157, 15);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Количество IP - адрессов:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label6.Location = new System.Drawing.Point(5, 172);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(114, 15);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "Количество узлов:";
+            // 
+            // label_IP
+            // 
+            this.label_IP.Location = new System.Drawing.Point(203, 108);
+            this.label_IP.Name = "label_IP";
+            this.label_IP.Size = new System.Drawing.Size(100, 15);
+            this.label_IP.TabIndex = 9;
+            // 
+            // label_SH
+            // 
+            this.label_SH.Location = new System.Drawing.Point(203, 130);
+            this.label_SH.Name = "label_SH";
+            this.label_SH.Size = new System.Drawing.Size(100, 13);
+            this.label_SH.TabIndex = 10;
+            // 
+            // label_KOL
+            // 
+            this.label_KOL.Location = new System.Drawing.Point(203, 152);
+            this.label_KOL.Name = "label_KOL";
+            this.label_KOL.Size = new System.Drawing.Size(100, 13);
+            this.label_KOL.TabIndex = 11;
+            // 
+            // label_Yz
+            // 
+            this.label_Yz.Location = new System.Drawing.Point(203, 174);
+            this.label_Yz.Name = "label_Yz";
+            this.label_Yz.Size = new System.Drawing.Size(100, 13);
+            this.label_Yz.TabIndex = 12;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(323, 233);
+            this.Controls.Add(this.label_Yz);
+            this.Controls.Add(this.label_KOL);
+            this.Controls.Add(this.label_SH);
+            this.Controls.Add(this.label_IP);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox_MASK);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.domainUpDown1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Click += new System.EventHandler(this.Form1_Click);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DomainUpDown domainUpDown1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox_MASK;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label_IP;
+        private System.Windows.Forms.Label label_SH;
+        private System.Windows.Forms.Label label_KOL;
+        private System.Windows.Forms.Label label_Yz;
+    }
+}
+
---
+++ dz_MASK/dz_MASK/Form1.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace dz_MASK
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            domainUpDown1.Text = "0";
+            textBox1.Text = "0.0.0.0";
+            textBox_MASK.Text = "0.0.0.0";
+        }
+        private void domainUpDown1_Click(object sender, EventArgs e)
+        {
+            int DUD = Convert.ToInt32(domainUpDown1.Text);
+            int DUD1 = 128;
+            int DUD2 = 192;
+            int DUD3 = 224;
+            int DUD4 = 240;
+            int DUD5 = 248;
+            int DUD6 = 252;
+            int DUD7 = 254;
+            int DUD8 = 255;
+            if (DUD == 0)
+            {
+                textBox_MASK.Text = "255.255.255.255";
+            }
+            if (DUD < 9)
+            {
+                if (DUD == 1) { textBox_MASK.Text = $"{DUD1}.0.0.0"; }
+                if (DUD == 2) { textBox_MASK.Text = $"{DUD2}.0.0.0"; }
+                if (DUD == 3) { textBox_MASK.Text = $"{DUD3}.0.0.0"; }
+                if (DUD == 4) { textBox_MASK.Text = $"{DUD4}.0.0.0"; }
+                if (DUD == 5) { textBox_MASK.Text = $"{DUD5}.0.0.0"; }
+                if (DUD == 6) { textBox_MASK.Text = $"{DUD6}.0.0.0"; }
+                if (DUD == 7) { textBox_MASK.Text = $"{DUD7}.0.0.0"; }
+                if (DUD == 8) { textBox_MASK.Text = $"{DUD8}.0.0.0"; }
+            }
+            if (8 < DUD & DUD < 17)
+            {
+                if (DUD == 9) { textBox_MASK.Text = $"255.{DUD1}.0.0"; }
+                else if (DUD == 10) { textBox_MASK.Text = $"255.{DUD2}.0.0"; }
+                else if (DUD == 11) { textBox_MASK.Text = $"255.{DUD3}.0.0"; }
+                else if (DUD == 12) { textBox_MASK.Text = $"255.{DUD4}.0.0"; }
+                else if (DUD == 13) { textBox_MASK.Text = $"255.{DUD5}.0.0"; }
+                else if (DUD == 14) { textBox_MASK.Text = $"255.{DUD6}.0.0"; }
+                else if (DUD == 15) { textBox_MASK.Text = $"255.{DUD7}.0.0"; }
+                else if(DUD == 16) { textBox_MASK.Text = $"255.{DUD8}.0.0"; }
+            }
+            if (16 < DUD & DUD < 25)
+            {
+                if (DUD == 17) { textBox_MASK.Text = $"255.255.{DUD1}.0"; }
+                else if (DUD == 18) { textBox_MASK.Text = $"255.255.{DUD2}.0"; }
+                else if (DUD == 19) { textBox_MASK.Text = $"255.255.{DUD3}.0"; }
+                else if (DUD == 20) { textBox_MASK.Text = $"255.255.{DUD4}.0"; }
+                else if (DUD == 21) { textBox_MASK.Text = $"255.255.{DUD5}.0"; }
+                else if (DUD == 22) { textBox_MASK.Text = $"255.255.{DUD6}.0"; }
+                else if (DUD == 23) { textBox_MASK.Text = $"255.255.{DUD7}.0"; }
+                else if (DUD == 24) { textBox_MASK.Text = $"255.255.{DUD8}.0"; }
+            }
+            else
+            {
+                if (DUD == 25) { textBox_MASK.Text = $"255.255.255.{DUD1}"; }
+                if (DUD == 26) { textBox_MASK.Text = $"255.255.255.{DUD2}"; }
+                if (DUD == 27) { textBox_MASK.Text = $"255.255.255.{DUD3}"; }
+                if (DUD == 28) { textBox_MASK.Text = $"255.255.255.{DUD4}"; }
+                if (DUD == 29) { textBox_MASK.Text = $"255.255.255.{DUD5}"; }
+                if (DUD == 30) { textBox_MASK.Text = $"255.255.255.{DUD6}"; }
+                if (DUD == 31) { textBox_MASK.Text = $"255.255.255.{DUD7}"; }
+                if (DUD == 32) { textBox_MASK.Text = $"255.255.255.{DUD8}"; }
+
+            }
+        }
+        public static IPAddress Chtoto(IPAddress ipAddress, IPAddress subnetMask)
+        {
+            byte[] ipBytes = ipAddress.GetAddressBytes();
+            byte[] maskBytes = subnetMask.GetAddressBytes();
+            byte[] networkBytes = new byte[4];
+            for (int i = 0; i < 4; i++)
+            {
+                networkBytes[i] = (byte)(ipBytes[i] & maskBytes[i]);
+            }
+            return new IPAddress(networkBytes);
+        }
+        private void Form1_Click(object sender, EventArgs e)
+        {
+            IPAddress ipAddress = IPAddress.Parse(textBox1.Text);
+            IPAddress subnetMask = IPAddress.Parse(textBox_MASK.Text);
+            IPAddress networkAddress = Chtoto(ipAddress, subnetMask);
+            label_IP.Text = networkAddress.ToString();
+
+            string networkAddress2 = textBox1.Text;
+            string subnetMask2 = textBox_MASK.Text;
+            // Проверка корректности ввода
+            if (!IPAddress.TryParse(networkAddress2, out IPAddress network) ||
+                !IPAddress.TryParse(subnetMask2, out IPAddress mask))
+            {
+                MessageBox.Show("Некорректный формат IP-адреса или маски!", "Ошибка");
+                return;
+            }
+            int addressCount = CalculateAddressCount(network, mask);
+            label_KOL.Text = addressCount.ToString();
+            label_Yz.Text = (addressCount-2).ToString();
+
+            GetBroadcastAddress();
+        }
+        private int CalculateAddressCount(IPAddress network, IPAddress mask)
+        {
+            // Преобразование IP-адресов в двоичный формат
+            byte[] maskBytes = mask.GetAddressBytes();
+            // Вычисление количества бит в маске
+            int maskBits = 0;
+            for (int i = 0; i < 4; i++)
+            {
+                maskBits += CountSetBits(maskBytes[i]);
+            }
+            // Вычисление количества доступных IP-адресов
+            int addressCount = (int)Math.Pow(2, 32 - maskBits); // -2 для учета сетевого и широковещательного адресов
+            return addressCount;
+        }
+        private int CountSetBits(byte b)
+        {
+            int count = 0;
+            while (b != 0)
+            {
+                count += b & 1;
+                b >>= 1;
+            }
+            return count;
+        }
+        private void GetBroadcastAddress()
+        {
+            string networkAddress = label_IP.Text;
+            string subnetMask = textBox_MASK.Text; 
+            // Преобразование в IP-адреса
+            IPAddress network = IPAddress.Parse(networkAddress);
+            IPAddress mask = IPAddress.Parse(subnetMask);
+            // Вычисление широковещательного адреса
+            byte[] networkBytes = network.GetAddressBytes();
+            byte[] maskBytes = mask.GetAddressBytes();
+            byte[] broadcastBytes = new byte[4];
+            for (int i = 0; i < 4; i++)
+            {
+                broadcastBytes[i] = (byte)(networkBytes[i] | (~maskBytes[i]));
+            }
+            IPAddress broadcastAddress = new IPAddress(broadcastBytes);
+            label_SH.Text = broadcastAddress.ToString();
+        }
+    }
+}
